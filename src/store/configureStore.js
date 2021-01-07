101bcdc915cadcf2560601521de77907ccba9373
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-01-07 08:59:06 +0100

    Review

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,10 @@
+// A place to register features
+
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
 import toast from "./middleware/toast";
-import api from "./middleware/api";
+import api from "./config/api";
 
 export default function () {
   return configureStore({
--- src/store/middleware/func.js
+++ src/store/middleware/func.js
@@ -1,6 +1,7 @@
 // This is the manually created to illustrate the middleware,
-// Redux-toolkit comes with middleware, juste have to pull it from @redux-toolkit
+// Redux-toolkit comes with middleware, just have to pull it from @redux-toolkit
 // And then configure that in configureStore.js
+// Do not call API in the middleware
 
 const func = ({ dispatch, getState }) => (next) => (action) => {
   if (typeof action === "function") action(dispatch, getState);
